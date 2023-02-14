b77d7514e300fe0218d08ad64d5d7994fcd56de4
Andrii Kudenko <kudenko4614@gmail.com>
2023-02-14 10:14:49 -0500

    Created the first class

---
+++ Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PayrollSystem
+{
+    public class Employee
+    {
+        private string _firstName;
+        private string _lastName;
+        private long _sinNumber;
+
+        Employee(string firstName, string lastName, long sinNumber) 
+        {
+            _firstName= firstName;
+            _lastName= lastName;
+            _sinNumber= sinNumber;
+        }
+    }
+}
