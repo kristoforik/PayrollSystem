8caa9534c68370293eb3bbfafd113d1efb47b393
hadikhurram <khurraab@shernet.sheridancollege.ca>
2023-02-14 10:46:07 -0500

    SalariedEmployee class implemented

--- SalariedEmployee.cs
+++ SalariedEmployee.cs
@@ -1,9 +1,23 @@
 ﻿using System;
+using Android.Accounts;
+using static AndroidX.ConstraintLayout.Core.Motion.Utils.HyperSpline;
+
 namespace PayrollSystem
 {
-	public class SalariedEmployee
+	public class SalariedEmployee: Employee 
 	{
-		
+		private double _amount;
+
+		public SalariedEmployee(string firstName, string lastName, long sinNumber, double amount) : base(firstName, lastName, sinNumber)
+		{
+			_amount = amount;
+		}
+
+		public double CalculateSalary(double amount)
+		{
+			return amount;
+		}
+          
 	}
 }
 
